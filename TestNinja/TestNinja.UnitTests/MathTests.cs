f1d5d01bbe9175ead2fdaf0678974079a7d8b6ba
srkans <serkansacma@hotmail.com>
2023-03-08 14:09:27 +0300

    sum of two numbers

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnsTheSumOfArguments()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+            //Act
+            var result = math.Add(1,2);
+            //Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
