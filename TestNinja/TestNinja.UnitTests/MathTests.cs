a1f0286877332be84c9f1aa024b977e916067ec0
srkans <serkansacma@hotmail.com>
2023-03-08 14:20:37 +0300

    tests for user defined math.Max

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -16,5 +16,45 @@ namespace TestNinja.UnitTests
             //Assert
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument() 
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+
+            //Act
+            var result = math.Max(2, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+
+            //Act
+            var result = math.Max(1, 2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnSameArgument()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+
+            //Act
+            var result = math.Max(1, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(1));
+        }
     }
 }
