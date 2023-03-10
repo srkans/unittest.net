0dbbf880bd7d56665ba27137ef596e61f861834a
srkans <serkansacma@hotmail.com>
2023-03-10 20:07:51 +0300

    stack.push method tests

---
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgumentNullException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(()=>stack.Push(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_Empty_ReturnZero()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+    }
+}
