b8f9b14d3b0531bcee5fd5ac2e31193916e71910
srkans <serkansacma@hotmail.com>
2023-03-10 09:05:03 +0300

    testing strings

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement() 
+        {
+            //Arrange
+            var formatter = new HtmlFormatter();
+            //Act
+            var result = formatter.FormatAsBold("abc");
+            //Assert
+            //Assert.That(result, Is.EqualTo("<strong>abc</strong>")); //specific way
+
+            Assert.That(result, Does.StartWith("<strong>")); //more general way
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result,Does.Contain("abc").IgnoreCase);
+            //case sensitive-- iptal etmek icin ignorecase
+        }
+    }
+}
--- TestNinja/TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
