de46f645ef92a4be5026b365e471317c2d9c4450
srkans <serkansacma@hotmail.com>
2023-03-10 20:23:52 +0300

    stack.Pop method tests

--- TestNinja/TestNinja.UnitTests/StackTests.cs
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -31,5 +31,42 @@ namespace TestNinja.UnitTests
 
             Assert.That(stack.Count, Is.EqualTo(0));
         }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(()=>stack.Pop(), Throws.TypeOf<InvalidOperationException>());
+
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObject_ReturnObjectOnTheTop()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            Assert.That(()=>stack.Pop(),Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObject_RemoveObjectOnTheTop()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            stack.Pop();
+
+            Assert.That(()=>stack.Peek(),Is.EqualTo("b"));
+
+
+        }
     }
 }
