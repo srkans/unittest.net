a94b0ae82a73f85389a5cb6394e4dbf2a19d164c
srkans <serkansacma@hotmail.com>
2023-03-14 16:21:52 +0300

    Testing Interaction between two object

---
+++ TestNinja/TestNinja.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder() 
+        {
+            //Arrange
+            var storage = new Mock<IStorage>();
+
+            var service = new OrderService(storage.Object);
+
+            //Act
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            //Assert
+            storage.Verify(s => s.Store(order)); //orderservice tarafından gelenle storage'daki aynı order mı?
+
+        }
+    }
+}
