a5b6a5742bbb63d50e4f034a49e2fd2d4e35c4c9
srkans <serkansacma@hotmail.com>
2023-03-14 15:25:35 +0300

    moq library has been implemented

--- TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
@@ -10,7 +11,13 @@ namespace TestNinja.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
         {
-            var service = new VideoService(new FakeFileReader());
+            var fileReader = new Mock<IFileReader>();
+
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+            //filereader read methodunu ilgili argument ile çağırdığımda boş bir string döndürecek bir mock set ettik.
+            //moq documentetion araştır.
+            //use moq for only external dependencies
+            var service = new VideoService(fileReader.Object);
 
             var result = service.ReadVideoTitle();
 
