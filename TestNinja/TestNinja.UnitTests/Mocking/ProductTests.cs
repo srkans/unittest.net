ffe40e0c94876825bd5128d461c6a6169e1ae72c
srkans <serkansacma@hotmail.com>
2023-03-14 16:40:35 +0300

    product discount

---
+++ TestNinja/TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿
+
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold= true });
+
+            Assert.That(result, Is.EqualTo(70));
+
+
+        }
+    }
+}
