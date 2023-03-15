6f2b284c51b8a502a881f810840651b01832c4b2
srkans <serkansacma@hotmail.com>
2023-03-15 18:41:33 +0300

    redirectresult tested

--- TestNinja/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -17,5 +17,16 @@ namespace TestNinja.UnitTests.Mocking
 
             storage.Verify(s=> s.DeleteEmployee(1));
         }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_RedirectResult()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            var result = controller.DeleteEmployee(1);
+
+           Assert.That(result,Is.TypeOf<RedirectResult>());
+        }
     }
 }
