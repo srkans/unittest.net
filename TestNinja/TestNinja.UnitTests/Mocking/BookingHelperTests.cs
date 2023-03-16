b0d59cf72f3adab0fc7d1b3f38c9515884b4b525
srkans <serkansacma@hotmail.com>
2023-03-16 10:06:24 +0300

    IBooking repository interface has been extracted

---
+++ TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelper_OverlappingBookingExistTests
+    {
+        [Test]
+        public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString() 
+        {
+            
+        }
+    }
+}
