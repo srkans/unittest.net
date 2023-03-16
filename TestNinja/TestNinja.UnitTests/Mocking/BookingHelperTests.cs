8c6345e9c8f9322e01a0d372a3d60288e133e40d
srkans <serkansacma@hotmail.com>
2023-03-16 14:59:24 +0300

    cancelled reservation

--- TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -126,6 +126,24 @@ namespace TestNinja.UnitTests.Mocking
             Assert.That(result, Is.Empty);
         }
 
+
+        [Test]
+        public void BookingOverlapButNewBookingIsCancelled_ReturnEmptyString()
+        {
+
+
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = After(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate),
+                Reference = "a",
+                Status = "Cancelled"
+            }, _repository.Object);
+
+            Assert.That(result, Is.Empty);
+        }
+
         private DateTime Before(DateTime dateTime, int days = 1)
         {
             return dateTime.AddDays(-days);
